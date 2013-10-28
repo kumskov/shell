f87ae4be32b2353e2770fbbffbd6690a7a1444db
kumskov <mishaelification@gmail.com>
2013-10-28 08:17:38 +0000

    Update executor.h

--- executor.h
+++ executor.h
@@ -11,27 +11,28 @@ void ironsInTheFire(char* command, char** args, int mode)   /* Properly working 
     pid=fork();
     if (pid)    
         {            /* Create new process with called execution mode (background or foreground) */
-        debug("I created child with PID %d\n", pid);
-        setpgid(pid, pid);                                                        
-    
-        createNewProcess(command, pid, mode);
-        if (mode==FG) sendToForeground(pid);                                             
-        if (mode==BG) sendToBackground(pid);
+        if ((!errno) || (errflag==-1))
+            {
+            debug("I created child with PID %d\n", pid);
+            setpgid(pid, pid);                                                        
+        
+            createNewProcess(command, pid, mode);
+            if (mode==FG) sendToForeground(pid);                                             
+            if (mode==BG) sendToBackground(pid);
+
+            restoreSignals(); /* Parent returns proper signals */
+            
+            }
 
         } else 
             {
             junk=setpgrp();   
             terminalReciever=getpid();
 
-            signal(SIGINT, SIG_DFL);    /* Parent returns proper signals */
-            signal(SIGQUIT, SIG_DFL);
-            signal(SIGTSTP, SIG_DFL);
-            signal(SIGTTIN, SIG_DFL);
-
             if (mode==FG) tcsetpgrp(TERMINAL, terminalReciever);    /* Allow child to use terminal */
             if (mode==BG) debug("Created a new child in background with ID of %d", terminalReciever);   /* This is totally ironic - terminalReciever DOESN'T RECIEVE ACCESS TO THE TERMINAL */
 
-            execvp(command, args);
+            errflag=execvp(command, args);
             exit(1);
             }
 
@@ -42,8 +43,9 @@ void ironsInTheFire(char* command, char** args, int mode)   /* Properly working 
 void sendToBackground(pid_t id) /* Take terminal control from process and indicate it in the table. Also this process will not be stopped. */
     {
     int num;
+    debug("Process %d will be sent to background", id);
     num=getProcessByID(id);
-    if (num!=-1) processTable[num]->currentStatus=BG;
+    if (num!=-1) processTable[num].currentState=BG;
         else killProcess(id);
     tcsetpgrp(TERMINAL, SHELL_ID);
     }
@@ -51,13 +53,16 @@ void sendToBackground(pid_t id) /* Take terminal control from process and indica
 void sendToForeground(pid_t id) /* This process will be in the foreground and will be stopped */
     {
     int num;
+    debug("Process %d will be sent to foreground", id);
     num=getProcessByID(id);
     if (num!=-1) 
         {
-        processTable[num]->currentStatus=FG;                   
-        tcsetpgrp(TERMINAL, id);                                
+        processTable[num].currentState=FG;
+        debug("I gave %d the terminal", id);                   
+        tcsetpgrp(TERMINAL, id);
         } else killProcess(id);
-    waitProcess(id);                                                                               
+    waitProcess(id);
+
     tcsetpgrp(TERMINAL, SHELL_ID);
     }
 
@@ -65,23 +70,27 @@ void waitProcess(pid_t id)  /* Terminate the process via waitpid */
     {
     int waiter;
     int status;
+    debug("Killing zombie process with id %d with wait", id);
     waiter=waitpid(id, &status, WNOHANG);
     deleteProcess(id);
+    debug("Zombie process %d succesfully killed", id);
     }
 
 void killProcess(pid_t id)  /* Terminate the process via SIGKILL */
     {
-    kill(id, SIGKILL); 
-    deleteProcess(id) ;
+    debug("Killing process with id %d by sending him SIGKILL", id);
+    kill(id, SIGKILL);
+    deleteProcess(id);
+    debug("Process %d succesfully killed", id);
     }
 
-void createNewProcess(char* name, pid_t id, int state); /* Add new process at the end of processTable */
+void createNewProcess(char* name, pid_t id, int state) /* Add new process at the end of processTable */
     {
     if (checkProcessLimit())
         {
-        processTable[processCounter]->name=name;
-        processTable[processCounter]->pid=id;
-        processTable[processCounter]->currentState=state;
+        processTable[processCounter].name=name;
+        processTable[processCounter].pid=id;
+        processTable[processCounter].currentState=state;
         processCounter++;
         }
     }
@@ -91,7 +100,7 @@ int getProcessByID(pid_t id)   /* Find process with id and return it's number in
     int i, flag=0;
     for (i=0;i<processCounter;i++)
         {   
-        if (processTable[i]->pid==id)
+        if (processTable[i].pid==id)
             {
             debug("Found process with id %d, being %d in the process list", id, i);
             flag=1;
@@ -111,15 +120,15 @@ void deleteProcess(pid_t id)   /* Delete process from execution list */
     int i, j, flag=0;
     for (i=0;i<processCounter;i++)
         {   
-        if (processTable[i]->pid==id)
+        if (processTable[i].pid==id)
             {
-            debug("Deleting %d process, being %d in the process list", id, i)
+            debug("Deleting %d process, being %d in the process list", id, i);
             processCounter--;  
             for (j=i;j<processCounter;j++)
                 {   
-                processTable[j]->name=processTable[j+1]->name;
-                processTable[j]->pid=processTable[j+1]->pid;
-                processTable[j]->currentState=processTable[j+1]->currentState;
+                processTable[j].name=processTable[j+1].name;
+                processTable[j].pid=processTable[j+1].pid;
+                processTable[j].currentState=processTable[j+1].currentState;
                 flag=1;
                 }
             }
@@ -151,6 +160,7 @@ void childControl()  /* To be finished  */
 
 void helpMe()   /* Print help */
     {
+    int j;
     for (j=0;j<commandAmount;j++)
         {
         printf("%s: %s\n", defaultCommands[j], commandHelp[j]);
