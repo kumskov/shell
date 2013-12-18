f2c90d3a8b71e12ba0744a7d1ad208d2b90bbb86
kumskov <mishaelification@gmail.com>
2013-12-18 08:06:48 +0000

    Update executor.h

--- executor.h
+++ executor.h
@@ -1,299 +1,365 @@
-#ifndef __executor_h__
-#define __executor_h__
+#ifndef __utils_h__
+#define __utils_h__
 
-/* Main procedure to launch new process. Because newProcess() is too mainstream. */
+#include "headers.h"
 
-void ironsInTheFire(char *cmd[], char* file, int mode, struct _IO_FILE *descriptor)   
+/* Old junk to work with lists. No longer needed since I don't use lists anymore. */
+
+void pushToList(arg** target, char* text) /* Will push "text" string to "trg"-list */
     {
-        pid_t pid;
-        pid=fork();
- 
-        if (pid)
+    /* int j=0, n=1; */
+    arg* safeCopy=*target;
+    arg* newNode;
+    newNode=(arg*) malloc(sizeof(arg));
+
+    while (safeCopy->next!=NULL)
         {
-            setpgid(pid, pid);                                                                      
-            procList=addNewProcess(pid, *(cmd), file, (int) mode); 
-            process* proc=getProcessByPID(pid);                            
-            if (mode == FG) sendProcessToForeground(proc);                                       
-            if (mode == BG) sendProcessToBackground(proc);   
-        } 
-        else
+        safeCopy=safeCopy->next;
+        }
+
+    newNode->elem=text;
+
+    /*
+    while (text[j]!='\0')
+            {
+            newNode->elem[j]=text[j];
+            j++;
+            n++;
+            printf("done\n");
+            newNode->elem=(char*) realloc(newNode->elem, n*sizeof(char));
+            }
+    */
+
+    debug("Arguement in push %s (temporal output)\n", newNode->elem);
+    /* newNode->elem[j]='\0';  */
+
+    safeCopy->next=newNode;
+    newNode->prev=safeCopy;
+    newNode->next=NULL;
+    }
+
+void initList(arg** target) /* Create the first node of arguement list. The first node will always be NULL */
+    {
+    (*target)=(arg*) malloc(sizeof(arg));
+    /* (*target)->elem=(char*) malloc(sizeof(char)); */
+    (*target)->elem='\0';
+    (*target)->next=NULL;
+    (*target)->prev=NULL;
+    }   
+
+void cleanList(arg** list)
+    {
+    arg* tail=*list;
+    arg* head=*list;
+    while (head->next!=NULL)
         {
-            signal(SIGINT, SIG_DFL);
-            signal(SIGQUIT, SIG_DFL);
-            signal(SIGTSTP, SIG_DFL);
+        head=head->next;
+        free(tail);
+        tail=head;
+        }
+    free(head);
+    free(tail);
+    }
+
+/* Now this is more interesting... */
+
+void init()
+    {
+
+        SHELL_ID = getpid();        
+        TERMINAL = STDIN_FILENO;     
+        SHELL_PG = getpgrp();                                  
+
+
+        if (isatty(TERMINAL)) 
+        {                    
+            while (tcgetpgrp(TERMINAL)!=(SHELL_PG=getpgrp())) kill(SHELL_ID, SIGTTIN);                   
+            signal(SIGQUIT, SIG_IGN);
+            signal(SIGTTOU, SIG_IGN);
+            signal(SIGTTIN, SIG_IGN);
+            signal(SIGTSTP, SIG_IGN);
+            signal(SIGINT, SIG_IGN);
             signal(SIGCHLD, &childHandler);
-            signal(SIGTTIN, SIG_DFL);
-            setpgrp();
-            if (mode==FG) tcsetpgrp(TERMINAL, getpid());
-            if (mode==BG) printf("Process No%d with PID <%d> was created.\n", ++processCounter, (int) getpid());
 
-            execute(cmd, file, mode, descriptor);
+            setpgid(SHELL_ID, SHELL_ID); 
 
-            exit(1);
-        }           
-    }
-
-void waitForProcess(process* proc)
-    {
-        int terminate;
-        waitpid(proc->pid, &terminate, WNOHANG);
-        procList=deleteProcess(proc);                                   
-    }
-
-void execute(char *cmd[], char *file, int mode, struct _IO_FILE *descriptor)
-    {
-        int commandDesc;
-        if (descriptor==STDIN) 
-        {
-            commandDesc=open(file, O_RDONLY, 0666);                                     
-            dup2(commandDesc, STDIN_FILENO);
-            close(commandDesc);
-        }
-
-        if (descriptor == STDOUT) 
-        {
-            commandDesc=open(file, O_CREAT|O_TRUNC|O_WRONLY, 0666);  // open (create) the file truncating it at 0, for write only
-            dup2(commandDesc, STDOUT_FILENO);
-            close(commandDesc);
-        }
-        if (execvp(cmd[0], cmd)==-1) log_err("\nCould not execute <%s>", *cmd);
-    }
-
-/* Pre-defined commands */
-
-void helpMe()   /* Print help */
-    {
-    int j;
-    for (j=0;j<commandAmount;j++)
-        {
-        printf("[%s]: %s\n", defaultCommands[j], commandHelp[j]);
-        }
-    }
-
-void findMe()   /* Get current directory */
-    {
-    char* env;
-    env=getenv("HOME");
-    printf("You are currently in: %s\n", env);
-    }
-
-void killMe()   /* Terminate shell execution */
-    {
-    printf("Terminating...\n");
-    exit(0);
-    }
-
-void changeDir()    /* Change directory */
-    {
-        /* if (arguements[1]=="..")
-        {
-            dotflag=1;
-        } */
-        if (arguements[1] == NULL) 
-        {
-            chdir(getenv("HOME"));                                                 
-        } 
-        else 
-        {
-            if (chdir(arguements[1])==-1) printf(" %s: Dir doesnt exits\n", arguements[1]);
-        }
-    }
-
-int Command_handler(char** split,char* shell)
-    {
-    int i = 0, executed = 0;
-    
-    signal(SIGINT,SIG_IGN);
-
-    while (split[i] != NULL)
-        {
-        if (strcmp(split[i],"$EUID") == 0)  /*$EUID*/      
+            SHELL_PG=getpgrp(); 
+            if (SHELL_ID!=SHELL_PG) 
             {
-            if (i == 0)
-                {
-                printf("EUID is: %d\n",geteuid());
-                executed = 1;
-                }
-            free(split[i]);
-            split[i] = (char *) malloc(sizeof(char)*10);
-            sprintf(split[i],"%d",geteuid());
-            }   
-
-        else if (strcmp(split[i],"$USER") == 0) /*$USER*/ 
-            {
-            if (i == 0)
-                {
-                printf("Your login is: %s\n",getenv("LOGNAME"));
-                executed = 1;
-                }
-            free(split[i]);
-            split[i] = (char *) calloc(strlen(getenv("LOGNAME"))+1,sizeof(char));
-            strncpy(split[i],getenv("LOGNAME"),strlen(getenv("LOGNAME")));
+                log_err("Shell isn't process group leader. Not gonna work under these conditions.");
+                exit(-1);
             }
+            tcsetpgrp(TERMINAL, SHELL_PG);
+            tcgetattr(TERMINAL, &TERMINAL_MODE);
+            currentDir=(char*) calloc(1024, sizeof(char));
+        }
+         else 
+        {
+            printf("ISATTY returned false. Sorry, mate, not going to work today.\n");
+            exit(-1);
+        }
+    }
 
-        else if (strcmp(split[i],"$HOME") == 0) /*$HOME*/
-            {
-            if (i == 0)
-                {
-                printf("Your home directory is: %s\n",getenv("HOME"));
-                executed = 1;
-                }
-            free(split[i]);
-            split[i] = (char *) calloc(strlen(getenv("HOME"))+1,sizeof(char));
-            strncpy(split[i],getenv("HOME"),strlen(getenv("HOME")));
-            }
 
-        else if (strcmp(split[i],"$SHELL") == 0)    /*$SHELL*/
-            {
-            if (i == 0)
-                {
-                printf("Shell is: %s\n",shell);
-                executed = 1;
-                }
-            free(split[i]);
-            split[i] = (char *) calloc(strlen(shell)+1,sizeof(char));
-            strncpy(split[i],shell,strlen(shell));
-            }
 
-        i++;
+int createPipeLine()
+{
+    pipeline=malloc(sizeof(process));
+    pproc *newp=pipeline;
+    pproc *nextp=malloc(sizeof(process));
+    int i=0;
+    int j=0;
+    int done=0;
+    while (arguements[i]=NULL)
+    {
+        if(arguements[i]==">")
+        {
+            newp->flout=arguements[i+1];
+            newp->foutflag=1;
+            i+=2;
+        } else
+        if (arguements[i]==">>")
+        {
+            newp->flout=arguements[i+1];
+            newp->fappflag=1;
+            i+=2;
         }
 
-    
-    i = 0;
-    if (executed == 0)
+        if (arguements[i]=="<")
         {
-
-        if (strcmp(split[i],"cd") == 0) /*cd*/
+            newp->flin=arguements[i+1];
+            newp->finflag=1;
+            i+=2;
+        }
+        
+        if (arguements[i]=="|")
+        {
+            newp->next=nextp;
+            newp->args[j]=NULL;
+            newp=newp->next;
+            done=1;
+            j=0;
+        } else
             {
-            if (split[i+1] != NULL)
-                {
-                printf("You're going to directory %s\n",split[i+1]);
-                if (chdir(split[i+1]) != 0)
-                    perror("Unreachable parameter of cd");
-                }
+                newp->args[j]=arguements[i];
+                i++;
+                j++;
+            }
+    }
+    newp->args[j]=NULL;
+
+    return done;
+}
+
+
+void startPiping(pproc* line)
+{
+    int fd[2];
+    pid_t pid;
+    if (line!=NULL)
+    {
+        if (line->foutflag==1) 
+        {
+            line->fout=open(line->flout, O_RDWR|O_CREAT|O_TRUNC, 0666);
+            dup2(line->fout, 1);
+        }
+        if (line->finflag==1) 
+        {
+            line->fin=open(line->flin, O_RDWR|O_CREAT|O_TRUNC, 0666);
+            dup2(line->fin, 0);
+        }
+        if (line->fappflag==1) 
+        {
+            line->fout=open(line->flout, O_RDWR|O_APPEND, 0666);
+            dup2(line->fout, 1);
+        }
+        if (line->next!=NULL)
+        {
+            pipe(fd);
+            pid=fork();
+            if (pid)
+            {
+                if ((line->fappflag==0) && (line->foutflag==0)) dup2(fd[1], 1);
+                execvp(line->args[0], line->args);
+                log_err();
+            }
             else
-                {
-                printf("You're going to home directory (%s).\n",getenv("HOME"));
-                chdir(getenv("HOME"));
-                }
-            }
-
-        else if (strcmp(split[i],"pwd") == 0)   /*pwd*/
             {
-            char* cur_dir = getcwd(NULL,0);
-            printf("Your current directory is: %s\n",cur_dir);
-            free(cur_dir);
+                dup2(fd[0], 0);
+                startPiping(line->next);
             }
-
-        else if (strcmp(split[i],"exit") == 0)  /*exit*/
+        } else
             {
-            printf("I'm going to exit.\n");
+                execvp(line->args[0], line->args);
+                log_err();
+            }
+    }
+
+}
+
+void cleanPipe()
+{
+    pproc* temp;
+    while(pipeline!=NULL)
+    {
+        temp=pipeline->next;
+        free(pipeline);
+        pipeline=temp;
+    }
+    free(temp);
+}
+        
+
+
+int checkProcessLimit()     /* Check if we have too many processes running */
+    {
+    if (processCounter>=MAX_PROCESS_AMOUNT) 
+        {
+        log_err("Maximum amount of parallel processes has been launched. Could not create another one.");
+        return 0;
+        } else
+            {
+            debug("There are %d processes at the moment. Creating a new one...", processCounter);
             return 1;
             }
-
-        else    /*Process*/
-            {
-            if (fork() == 0)    /*Son*/
-                {
-                signal(SIGINT,SIG_DFL);
-                printf("Starting process with PID = %d\n",getpid());
-                execvp(split[i],split);
-                printf("There is no such command.\n");
-                exit(0);
-                }
-            else    /*Parent*/
-                {
-                }
-            }
-        }
-    
-    wait(NULL);
-    return 0;       
     }
 
-int checkDefaultCommands()  /* Check for pre-defined command usage. Almost done */
+void sendProcessToForeground(process* proc)
     {
-    int chk, f=0, i;
-    for (i=0;i<commandAmount;i++)
+        proc->status=FG;                                                
+        tcsetpgrp(TERMINAL, proc->pid);                                 
+        waitForProcess(proc);                                                                              
+        tcsetpgrp(TERMINAL, SHELL_PG);                              
+    }
+
+void sendProcessToBackground(process* proc)
+    {
+        /*
+        That's a huge load o' nothing to see here.
+        Like - really. This is only for part 4. I'll do it later. And see if it works later.
+        */
+        if (proc==NULL) return; 
+        tcsetpgrp(TERMINAL, SHELL_PG);                            
+    }
+
+process* addNewProcess (pid_t pid, char* name, char* descriptor, int status)
+    {
+            process *newProc=malloc(sizeof(process));
+            newProc->name=(char*) malloc(sizeof(name));
+            newProc->name=name;
+            newProc->pid=pid;
+            newProc->status=status;
+            newProc->descriptor=(char*) malloc(sizeof(descriptor));
+            newProc->descriptor=descriptor;
+            newProc->next=NULL;
+
+            if (procList==NULL) 
+            {
+                processCounter++;
+                newProc->cnt=processCounter;
+                return newProc;
+            } 
+            else 
+            {
+                process *temp=procList;
+
+                while (temp->next!=NULL) temp=temp->next;
+
+                newProc->cnt=temp->cnt+1;
+                temp->next=newProc;
+                processCounter++;
+
+                return procList;
+            }
+    }
+
+int changeProcessStatus(pid_t pid, int status)
+    {
+        process *proc=getProcessByPID(pid);
+        if (proc==NULL) return FALSE;
+        else 
         {
-        chk=strcmp(arguements[0], defaultCommands[i]);
-        /* printf("%s - %s: %d\n", arguements[0], defaultCommands[i], chk); */
-        if (!chk) 
-            {
-            switch(i)
-                {
-                case 0:
-                    {
-                    killMe();
-                    f=1;
-                    break;
-                    }
-                case 1:
-                    {
-                    findMe();
-                    f=1;
-                    break;
-                    }
-                case 2:
-                    {
-                    helpMe();
-                    f=1;
-                    break;
-                    }
-                case 3:
-                    {
-                    printf("---\n");
-                    f=1;
-                    break;
-                    }
-                case 4:
-                    {
-                    changeDir();
-                    f=1;
-                    break;
-                    }
-                case 5:
-                    {
-                    printf("Work in progress. Sorry, mate.\n");
-                    f=1;
-                    break;
-                    }
-                case 6:
-                    {
-                    ironsInTheFire(arguements+2, *(arguements+1), FG, STDIN);
-                    f=1;
-                    break;
-                    }
-                case 7:
-                    {
-                    ironsInTheFire(arguements+2, *(arguements+1), FG, STDOUT);
-                    f=1;
-                    break;
-                    }   
-                case 8:
-                    {
-                    printf("Work in progress. Sorry, mate.\n");
-                    f=1;
-                    break;
-                    }   
-                default:
-                    {
-                    break;
-                    }
-
-                }
-            }
+            proc->status=status;
+            return TRUE;
         }
-
-    return f;
-
     }
 
-void startWorking()         /* Yup. The creation of a procedure for 1 line of code was totally nessesary */
+process* deleteProcess(process* proc)
     {
-        if(!checkDefaultCommands()) {} ironsInTheFire(arguements, "standart_output", FG, 0);
-        sleep(1);
+        if (procList==NULL) return NULL;
 
+        process* currentProcess;
+        process* beforeProcess;
+
+        currentProcess=procList->next;
+        beforeProcess=procList;
+
+        if (beforeProcess->pid==proc->pid) 
+        {
+            beforeProcess=beforeProcess->next;
+            processCounter--;
+            return currentProcess;
+        }
+
+        while (currentProcess!=NULL) 
+        {
+            if (currentProcess->pid==proc->pid) 
+            {
+                processCounter--;
+                beforeProcess->next=currentProcess->next;
+            }
+            beforeProcess=currentProcess;
+            currentProcess=currentProcess->next;
+        }
+        return procList;
+    }
+
+process* getProcessByPID(pid_t pid)
+    {
+        process* proc=procList;
+        while (proc!=NULL) 
+        {
+            if (proc->pid==pid) return proc;
+            else proc=proc->next;
+        }          
+        return NULL;
+    }
+
+void childHandler(int p)
+    {
+        pid_t pid;
+        int terminationStatus;
+        pid=waitpid(WAIT_ANY, &terminationStatus, WUNTRACED|WNOHANG); // intercept the process that sends the signal
+        if (pid) 
+        {                                                                       
+            process* proc = getProcessByPID(pid);                      
+            if (proc==NULL) return;
+            if (WIFEXITED(terminationStatus)) 
+            {                                                      
+                printf("\n[%d]: <%s> finished succesfully\n", proc->id, proc->name); 
+                procList=deleteProcess(proc);                                                    
+            }
+            else 
+            if (WIFSIGNALED(terminationStatus)) 
+            {                                  
+                printf("\n[%d]: <%s> was killed\n", proc->id, proc->name); 
+                procList=deleteProcess(proc);                                                     
+            } 
+            else 
+                if (WIFSTOPPED(terminationStatus))  /* Sigstp */
+                {                                                                                                 
+                    tcsetpgrp(TERMINAL, proc->pid);                   
+                    printf("\n[%d]: %s was stopped\n", processCounter, proc->name); 
+                } 
+                else 
+                {
+                    if (proc->status==BG) 
+                    {                   
+                        procList=deleteProcess(proc);
+                    }
+                }
+            tcsetpgrp(TERMINAL, SHELL_PG);
+        }
     }
 
 #endif
